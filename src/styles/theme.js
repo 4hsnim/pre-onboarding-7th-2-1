27deb62f8366ea064dde419953a2446d3d6b1218
4hsnim <4hsnim@naver.com>
2022-11-02 23:42:48 +0900

    feat: 테마 및 글로벌스타일 설정

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const defaultTheme = {
+  txtColor: {
+    default: "#000000",
+    accent: "#FFFFFF",
+  },
+  btnColor: {
+    default: "#D9D9D9",
+    filtered: "#000000",
+    accent: "#0094FF",
+  },
+  bgColor: {
+    default: "#FFFFFF",
+    accent: "#0094FF",
+  },
+  borderColor: {
+    default: "#000000",
+  },
+};
+
+export default defaultTheme;
